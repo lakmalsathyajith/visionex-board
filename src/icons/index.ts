3d2a40c3f8824e8452e87a90dd86f373cab7184b
lakmalsathyajith <slakmal68@gmail.com>
2024-12-17 17:04:32 +0530

    create icon exports

---
+++ src/icons/index.ts
@@ -0,0 +1,37 @@
+import ArrowDown from "./ArrowDown";
+import ArrowUp from "./ArrowUp";
+import ArrowLeft from "./ArrowLeft";
+import ArrowRight from "./ArrowRight";
+import CalendarIcon from "./CalendarIcon";
+import DashboardIcon from "./DashboardIcon";
+import DotsIcon from "./DotsIcon";
+import FlashIcon from "./FlashIcon";
+import FolderIcon from "./FolderIcon";
+import InfoCircleIcon from "./InfoCircleIcon";
+import LinkIcon from "./LinkIcon";
+import MessageIcon from "./MessageIcon";
+import PencilIcon from "./PencilIcon";
+import Plus2Icon from "./Plus2Icon";
+import SignOutIcon from "./SignOutIcon";
+import UserIcon from "./UserIcon";
+import UserProfileIcon from "./UserProfileIcon";
+
+export {
+  ArrowDown,
+  ArrowUp,
+  ArrowLeft,
+  ArrowRight,
+  CalendarIcon,
+  DashboardIcon,
+  DotsIcon,
+  FlashIcon,
+  InfoCircleIcon,
+  LinkIcon,
+  MessageIcon,
+  PencilIcon,
+  SignOutIcon,
+  UserProfileIcon,
+  Plus2Icon,
+  UserIcon,
+  FolderIcon,
+};
